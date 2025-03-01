abd8934c11dc94e52f3220c2c27b79d7e92c070f
box1bs <box.1abstract1@gmail.com>
2025-03-01 18:09:23 +0300

    minor fixes

--- pkg/treeIndex/treeNode.go
+++ pkg/treeIndex/treeNode.go
@@ -1,25 +1,20 @@
 package tree
 
 import (
-	"sync"
 )
 
 type TreeNode struct {
 	url    string
-	childs []*TreeNode
+	children []*TreeNode
 }
-
-var mu sync.Mutex
 
 func NewNode(url string) *TreeNode {
 	return &TreeNode{
 		url:    url,
-		childs: make([]*TreeNode, 0),
+		children: make([]*TreeNode, 0),
 	}
 }
 
 func (n *TreeNode) AddChild(child *TreeNode) {
-	mu.Lock()
-	defer mu.Unlock()
-	n.childs = append(n.childs, child)
+	n.children = append(n.children, child)
 }
